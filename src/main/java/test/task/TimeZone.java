c6f92d74548d2bc9f2e2067a39c9fce48ba1ce5b
Daniil Martynov <DaniilDeFacto@yandex.ru>
2024-02-07 21:56:11 +0400

    Add TimeZone.java

---
+++ src/main/java/test/task/TimeZone.java
@@ -0,0 +1,10 @@
+package test.task;
+
+import java.util.Map;
+
+public class TimeZone {
+    public static final Map<String, String> IDS = Map.of(
+            "Владивосток", "Asia/Vladivostok",
+            "Тель-Авив", "Asia/Jerusalem"
+    );
+}
