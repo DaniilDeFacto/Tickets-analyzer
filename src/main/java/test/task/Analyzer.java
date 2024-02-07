98e1c3ecde9636b0225034d35c69e3e39013a10a
Daniil Martynov <DaniilDeFacto@yandex.ru>
2024-02-07 21:58:36 +0400

    Add Analyzer.java and AnalyzerTest.java

---
+++ src/main/java/test/task/Analyzer.java
@@ -0,0 +1,51 @@
+package test.task;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Analyzer {
+    public static Map<String, Duration> findMinFlightTimes(List<Ticket> tickets, String origin, String destination) {
+        Map<String, Duration> minFlightTimes = new HashMap<>();
+        for (Ticket ticket : tickets) {
+            if (ticket.getOrigin().equals(origin) && ticket.getDestination().equals(destination)) {
+                var carrier = ticket.getCarrier();
+                var flightDuration = Duration.between(ticket.getDepartureDateTime(), ticket.getArrivalDateTime());
+                var currentMinDuration = minFlightTimes.getOrDefault(carrier, Duration.ofDays(365));
+                if (flightDuration.compareTo(currentMinDuration) < 0) {
+                    minFlightTimes.put(carrier, flightDuration);
+                }
+            }
+        }
+        return minFlightTimes;
+    }
+
+    public static double calculatePriceDifference(List<Ticket> tickets, String origin, String destination) {
+        var prices = tickets.stream()
+                .filter(ticket -> ticket.getOrigin().equals(origin) && ticket.getDestination().equals(destination))
+                .map(Ticket::getPrice)
+                .sorted()
+                .collect(Collectors.toList());
+        var averagePrice = prices.stream()
+                .mapToDouble(Double::doubleValue)
+                .average()
+                .orElse(0);
+        var medianPrice = calculateMedian(prices);
+        return averagePrice - medianPrice;
+    }
+
+    public static double calculateMedian(List<Double> list) {
+        int size = list.size();
+        if (size % 2 == 0) {
+            return Stream.of(list.get(size / 2), list.get(size / 2 - 1))
+                    .mapToDouble(Double::doubleValue)
+                    .average()
+                    .orElse(0);
+        } else {
+            return list.get(size / 2);
+        }
+    }
+}
---
+++ src/test/java/test/task/AnalyzerTest.java
@@ -0,0 +1,38 @@
+package test.task;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AnalyzerTest {
+
+    private static List<Ticket> tickets;
+
+    @BeforeAll
+    static void setup() {
+        tickets = JsonReader.readTicketsFromJson("/tickets.json");
+    }
+
+    @Test
+    void findMinFlightTimesTest() {
+        var minFlightTimes = Analyzer.findMinFlightTimes(tickets, "VVO", "TLV");
+        assertEquals(4, minFlightTimes.size());
+        assertTrue(minFlightTimes.containsKey("TK"));
+        assertTrue(minFlightTimes.containsKey("S7"));
+        assertTrue(minFlightTimes.containsKey("SU"));
+        assertTrue(minFlightTimes.containsKey("BA"));
+        assertEquals(12, minFlightTimes.get("TK").toHours());
+        assertEquals(13, minFlightTimes.get("S7").toHours());
+        assertEquals(13, minFlightTimes.get("SU").toHours());
+        assertEquals(15, minFlightTimes.get("BA").toHours());
+    }
+
+    @Test
+    void calculatePriceDifferenceTest() {
+        double priceDifference = Analyzer.calculatePriceDifference(tickets, "VVO", "TLV");
+        assertEquals(460, priceDifference);
+    }
+}
